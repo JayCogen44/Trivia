8d850fbb4e631ea1f499cd2b301ec58502271225
Harmon Huynh <harmonh@gmail.com>
2019-03-05 20:10:40 -0800

    Finished setting up facebook strategy

--- server/server.js
+++ server/server.js
@@ -1,18 +1,40 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const authController = require('./controllers/authController');
+
+require('./controllers/authController');
 
 const app = express();
 const PORT = 3000;
 
-
 app.use(bodyParser.json());
 
-app.get('/', (req,res) => {
-    res.json('hello Jonathan');
-})
+app.get('/', (req, res) => {
+  res.json('hello Jonathan');
+});
 
+const authRouter = express.Router();
 
-app.listen(PORT, function () {
-    console.log(`listening on port ${PORT}`);
-}); //listens on port 3000 -> http://localhost:3000/
+authRouter.get('/facebook', passport.authenticate('facebook'), (req, res) => {
+  console.log('Logging in with Facebook');
+});
+
+authRouter.get(
+  '/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect();
+  }
+);
+
+app.use('/auth', authRouter);
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+}); // listens on port 3000 -> http://localhost:3000/
