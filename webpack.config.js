a16c6e092d7df1ba01b3f3ec9328637f7ed047d6
Harmon Huynh <harmonh@gmail.com>
2019-03-07 10:41:56 -0800

    Updated webpack config to load scss

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,45 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: './client/index.js',
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, './build'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-        open: true,
-        contentBase: './',
-        publicPath: '/build/',
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                pathRewrite: { '^/api': '' }
-            }
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-                resolve: {
-                    extensions: ['*', '.js', '.jsx'],
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: 'style-loader' },
-                    { loader: 'css-loader' },
-                   
-                ]
-            }
-        ]
+  entry: './client/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    open: true,
+    contentBase: './',
+    publicPath: '/build/',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        pathRewrite: { '^/api': '' }
+      }
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+        resolve: {
+          extensions: ['*', '.js', '.jsx']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      },
+      {
+        test: /\.scss$/,
+        use: {
+          loader: 'scss-loader'
+        }
+      }
+    ]
+  }
 };
