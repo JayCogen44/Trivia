b73bcc3f36f2aa3ab5038824d90598c1e3bedb06
Harmon Huynh <harmonh@gmail.com>
2019-03-07 13:15:50 -0800

    Facebook returns data

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,12 +7,25 @@ const pool = new Pool({
   connectionString: process.env.DB_CONNECT
 });
 
+const addUserQuery = `INSERT INTO users(name, )`;
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
 passport.use(
   new FacebookStrategy(
     {
       clientID: process.env.FB_APPID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: 'http://localhost:3000/auth/facebook/callback'
+      callbackURL: 'http://localhost:3000/auth/facebook/callback',
+      profileFields: ['id', 'emails', 'name', 'picture.type(normal)']
     },
     (accessToken, refreshToken, profile, done) => {
       console.log('ACCESS TOKEN', accessToken);
